32677cf787ee9ee3360c53d60e4c233510ee052a
Kyle Li <likyle701@gmail.com>
2025-02-19 03:50:03 -0800

    Initial Commit

---
+++ Player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed : float
+
+func _physics_process(delta: float) -> void:
+	var walk = Input.get_axis("left", "right")
+	
+	position.x += walk * speed
+	
+	
+	
