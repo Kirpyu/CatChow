1eb2c1af96201c7a406f37fa857f0879073d7929
TangInasal1 <149883153+TangInasal1@users.noreply.github.com>
2025-02-23 00:03:28 +0800

    Update sink.gd

--- Counter/Sink/sink.gd
+++ Counter/Sink/sink.gd
@@ -1,31 +1,41 @@
 extends Counter
 
-var current_state: STATES = STATES.IDLE
-
 enum STATES{
 	IDLE,
 	WORKING
 }
 
+var current_state: STATES = STATES.IDLE
+
 func _ready() -> void:
 	match_animation()
 
 func do_task():
-	pass 
-#	Remove pass when you finish
-#	very similar to what you did yesterday
-#	you can also look at stove top, instead of taking in patty, you have to take dirty dishes and return clean dishes
-#	Dishes instead
-#	Check if popped_item = InventoryManager.ITEMS.DIRTY_DISH, added to inventory item should be CLEAN_DISH
-#	Make sure dirty dishes are set to finite and only has 3, if you leave it default at 0 it would bug
+	if current_state == STATES.WORKING:
+		print("Working")
+		return
+		
+	if InventoryManager.inventory.is_empty():
+		error_message()
+		return	
+		
+	var item: Item = InventoryManager.inventory.back()
+	if item.item_name == InventoryManager.ITEMS.DIRTYDISH:
+		InventoryManager.remove_inventory()
+		work_timer.start()
+		current_state = STATES.WORKING
+		match_animation()
+	else:
+		error_message()
 
+func _on_work_timer_timeout() -> void:
+	current_state = STATES.IDLE
+	InventoryManager.add_storage(InventoryManager.ITEMS.CLEANDISH)
+	match_animation()
+			
 func match_animation() -> void:
 	match current_state:
 		STATES.IDLE:
 			animated_sprite.play("default")
 		STATES.WORKING:
 			animated_sprite.play("working")
-
-func _on_work_timer_timeout() -> void:
-	pass
-#	complete this also, with match animation this time, copy what you did with the cutting board
